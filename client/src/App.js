b0d12e00b9fcde1b32e1266806553bb6302c6b06
Brian Jason <holaymolay@Brians-MacBook-Pro.local>
2020-10-06 09:44:15 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <div>
+        hello react
+      </div>
+      <div>
+        hello react-router
+      </div>
+    </div>
+  );
+}
+
+export default App;
