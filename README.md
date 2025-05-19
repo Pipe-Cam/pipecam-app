f06f858439c9f530e17e052bdcd68f307f947ac9
Brian Jason <holaymolay@users.noreply.github.com>
2025-05-19 16:27:13 -0700

    test: configure jest with supertest

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # fullstack-boilerplate
 [node + express + mongo] backend api &amp; [react] frontend
+
+## Testing
+
+The API uses [Jest](https://jestjs.io/) with [Supertest](https://github.com/ladjs/supertest)
+for endpoint testing.
+
+From the `api` directory run:
+
+```bash
+npm test
+```
+
+This will execute all tests under `api/test`.
--- api/test/root.test.js
+++ api/test/root.test.js
@@ -1,9 +1,9 @@
-const request = require('supertest');
-const app = require('../app');
+// `request` is provided by the Jest setup file
+
 
 describe('GET /', () => {
   it('responds with expected text', async () => {
-    const res = await request(app).get('/');
+    const res = await request.get('/');
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe('Hey! Quit snooping. Go away and mind your own business!');
   });
