310db78ce6e195a5b37fcca3101543e72625ae16
Brian Jason <holaymolay@users.noreply.github.com>
2025-05-19 16:44:32 -0700

    Remove mongoose usage

--- api/db/models/InspectionModel.js
+++
@@ -1,9 +0,0 @@
-var {mongoose} = require('../mongodbConnect')
-const inspectionSchema = require('../schemas/inspectionSchema')
-const InspectionSchema = inspectionSchema.new(mongoose)
-const InspectionModel = mongoose.model('inspections', InspectionSchema);  
-
-module.exports = {
-    model: InspectionModel, 
-    schema: InspectionSchema
-}
