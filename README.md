6a7059f3b7a7bbd2b51723171f1bc4a368c0e820
Brian Jason <holaymolay@users.noreply.github.com>
2025-05-19 16:43:54 -0700

    Switch to Supabase

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fullstack-boilerplate
 [node + express + mongo] backend api &amp; [react] frontend
+
+## Environment Variables
+
+The API expects the following variables to be available at runtime:
+
+```
+SUPABASE_URL=<your-supabase-project-url>
+SUPABASE_KEY=<your-supabase-service-key>
+```
+
+These values are used to initialize the Supabase client in `api/db/supabaseClient.js`.
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,183 +1,146 @@
 var express = require('express');
 var router = express.Router();
 
-const DB = require('../db/dbAssets')
-
+const DB = require('../db/dbAssets');
 
 /* GET ROUTES */
-
-router.get('/', function(req, res, next) {
-  res.send('Hey! Quit snooping. Go away and mind your own business!')
+router.get('/', function (req, res) {
+  res.send('Hey! Quit snooping. Go away and mind your own business!');
 });
 
-router.get('/client/:id', function(req, res, next) {
-  DB.client.model.find({ _id: req.params.id }, function (err, response) {
-    if (err) {
-      console.log(err)
-    } else {
-      res.json(response)
-    }
-  })
-});
-
-router.get('/inspection/:id', function(req, res, next) {
-  DB.inspection.model.find({ _id: req.params.id }, function (err, response) {
-    if (err) {
-      console.log(err)
-      res.status(500).send()
-    } else {
-      console.log(response)
-      res.status(200).json(response)
-    }
-  })
-});
-
-router.get('/recent-clients', function(req, res, next) {
-  DB.client.model
-    .find({ client_status: 'active' })
-    .sort('-last_modified')
-    .limit(10)
-    .exec(function (err, response) {
-      // console.log(response)
-      res.status(200).json(response)
-    })
-});
-
-router.get('/archived-clients', function(req, res, next) {
-  DB.client.model.find({ client_status: 'archived' }).exec(function (err, response) {
-    // console.log(response)
-    res.status(200).json(response)
-  })
-});
-
-router.get('/search-for-client', function(req, res, next) { 
-  if (req.query.client_search) {
-    const regex = new RegExp(escapeRegex(req.query.client_search), 'gi');
-
-    DB.client.model.find({ "business_name": regex }, function(err, foundclients) {
-        if(err) {
-            console.log(err);
-        } else {
-          if (!foundclients.length) {
-            DB.client.model.find({ contact_name: regex }, function (err, foundclients2) {
-              if (err) {
-                console.log(err)
-              } else {
-                if (!foundclients2.length) {
-                  res.send('no such client exists')
-                } else {
-                  res.json(foundclients2)
-                }
-              }
-            })
-          } else {
-            res.json(foundclients)
-          }
-        }
-    }); 
-  } else {
-    res.send('query failed')
+router.get('/client/:id', async function (req, res) {
+  try {
+    const data = await DB.client.getById(req.params.id);
+    res.json(data || {});
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
   }
 });
 
-router.get('/scheduled-inspections', function(req, res, next) {
-  DB.inspection.model
-    .find({ status: 'scheduled_inspection' })
-    .sort('overview.inspection_date')
-    .limit(20)
-    .exec(function (err, response) {
-      // console.log(response)
-      res.status(200).json(response)
-    })
+router.get('/inspection/:id', async function (req, res) {
+  try {
+    const data = await DB.inspection.getById(req.params.id);
+    res.status(200).json(data || {});
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
 });
 
-router.get('/recent-inspections', function(req, res, next) {
-  DB.inspection.model
-    .find({ status: ['active_inspection', 'completed_inspection'] })
-    .sort('-overview.inspection_date')
-    .limit(20)
-    .exec(function (err, response) {
-      // console.log(response)
-      res.status(200).json(response)
-    })
+router.get('/recent-clients', async function (req, res) {
+  try {
+    const data = await DB.client.getRecent();
+    res.status(200).json(data || []);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
+});
+
+router.get('/archived-clients', async function (req, res) {
+  try {
+    const data = await DB.client.getArchived();
+    res.status(200).json(data || []);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
+});
+
+router.get('/search-for-client', async function (req, res) {
+  if (!req.query.client_search) {
+    return res.send('query failed');
+  }
+  try {
+    const data = await DB.client.search(req.query.client_search);
+    if (!data || data.length === 0) {
+      res.send('no such client exists');
+    } else {
+      res.json(data);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
+});
+
+router.get('/scheduled-inspections', async function (req, res) {
+  try {
+    const data = await DB.inspection.getScheduled();
+    res.status(200).json(data || []);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
+});
+
+router.get('/recent-inspections', async function (req, res) {
+  try {
+    const data = await DB.inspection.getRecent();
+    res.status(200).json(data || []);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
 });
 
 /* POST ROUTES */
-router.post('/new-client', function(req, res, next) {
-  DB.client.new(req.body)
-  res.send('new client added')
+router.post('/new-client', async function (req, res) {
+  await DB.client.new(req.body);
+  res.send('new client added');
 });
 
-router.post('/new-inspection', function(req, res, next) {
-  console.log(req.body)
-  DB.inspection.new(req.body)
-  res.send('new inspection added')
+router.post('/new-inspection', async function (req, res) {
+  await DB.inspection.new(req.body);
+  res.send('new inspection added');
 });
-
 
 /* PUT ROUTES */
-router.put('/inspection/:id', function(req, res, next) {
-    let id = req.params.id
-    console.log(id)
-    let bodyData = req.body
-    console.log("body", req.body)
-
-    DB.inspection.model.findOneAndUpdate({_id: id}, {$set: bodyData}, function(err, updateData){
-      if(err){
-        console.log(err)
-        res.status(500).send('update failed')
-      } else {
-        console.log(Object.keys(updateData))
-        res.status(200).json(updateData)
-      }
-    })
-  })
-//     DB.inspection.model.findById(id, function(err, data){
-// });
-
-router.put('/client-all', function(req, res, next) {
-  DB.client.model.updateMany({client_status: 'Active'}, {client_status: 'active'}, function(err){
-    if(err){
-      console.log(err)
-      return res.status(500).send("delete inspection: failure");
-    } else {
-      return res.status(200).send("delete inspection: success");
-    }
-  });
+router.put('/inspection/:id', async function (req, res) {
+  try {
+    const data = await DB.inspection.update(req.params.id, req.body);
+    res.status(200).json(data || {});
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('update failed');
+  }
 });
 
-router.put('/client/:id', function(req, res, next) {
-  DB.client.update(req.params.id, req.body)
-  res.send('client updated')
+router.put('/client-all', async function (req, res) {
+  try {
+    await DB.client.bulkActivate();
+    res.status(200).send('delete inspection: success');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('delete inspection: failure');
+  }
+});
+
+router.put('/client/:id', async function (req, res) {
+  await DB.client.update(req.params.id, req.body);
+  res.send('client updated');
 });
 
 /* DELETE ROUTES */
-router.delete('/inspection/:id', function(req, res, next) {
-  DB.inspection.model.findOneAndDelete({_id: req.params.id}, function(err){
-    if(err){
-      console.log(err)
-      return res.status(500).send("delete inspection: failure");
-    } else {
-      return res.status(200).send("delete inspection: success");
-    }
-  });
+router.delete('/inspection/:id', async function (req, res) {
+  try {
+    await DB.inspection.delete(req.params.id);
+    res.status(200).send('delete inspection: success');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('delete inspection: failure');
+  }
 });
 
-router.delete('/client/:id', function(req, res, next) {
-  DB.client.model.findOneAndUpdate({_id: req.params.id}, {client_status: 'archived', last_modified: new Date()}, function(err){
-    if(err){
-      console.log(err)
-      return res.status(500).send("delete client: failure");
-    } else {
-      return res.status(200).send("delete client: success");
-    }
-  });
+router.delete('/client/:id', async function (req, res) {
+  try {
+    await DB.client.archive(req.params.id);
+    res.status(200).send('delete client: success');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('delete client: failure');
+  }
 });
-
 
 module.exports = router;
-
-/* HELPER FUNCTIONS */
-function escapeRegex(text) {
-  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-};
